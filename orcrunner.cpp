b75e912c4e1700daeb7084e232f66dae1f743b76
ArthurG0 <agatin@umail.ucsb.edu>
2020-02-28 19:35:04 -0800

    challenge without jump is complete, flag retrieved

--- orcrunner.cpp
+++ orcrunner.cpp
@@ -24,6 +24,132 @@ struct instruction{
     }
 };
 
+int* registers;
+std::string our_stdout;
+
+void executeAdd(instruction i){
+
+}
+void executeSub(instruction i){
+
+}
+void executeMul(instruction i){
+
+}
+void executeDiv(instruction i){
+
+}
+void executeCmp(instruction i){
+
+}
+void executeTst(instruction i){
+
+}
+void executeAnd(instruction i){
+
+}
+void executeOrr(instruction i){
+    //cout << "executing orr" << endl;
+    if(i.firstcode == "010"){
+        registers[stoi(i.dest,NULL,2)] = registers[stoi(i.dest, NULL,2)] | registers[stoi(i.src, NULL, 2)];
+    }
+    else if(i.firstcode == "011"){
+        //cout << i.src << endl;
+
+        //Arthur - 
+        //why is it the substring of 0 to 7? IDK, but the instruction is 
+        //ORR+011011100000001101100000000000000000000000
+        //src is 1101100000000000000000000000, which we need to do a bitwise OR on. I think we are supposed to look at the first byte?
+        //it works if you only look at the first 7 characters.
+        registers[stoi(i.dest,NULL,2)] = registers[stoi(i.dest, NULL,2)] | stoi(i.src.substr(0,7), NULL, 2);
+    }
+    else{
+        cout << "problem with executeOrr" << endl;
+    }
+
+}
+void executeXor(instruction i){
+    //cout << "executing xor" << endl;
+    if(i.firstcode == "010"){
+        registers[stoi(i.dest,NULL,2)] = registers[stoi(i.dest, NULL,2)] ^ registers[stoi(i.src, NULL, 2)];
+    }
+    else if(i.firstcode == "011"){
+        registers[stoi(i.dest,NULL,2)] = registers[stoi(i.dest, NULL,2)] ^ stoi(i.src, NULL, 2);
+    }
+    else{
+        cout << "problem with executeXor" << endl;
+    }
+
+}
+void executeStr(instruction i){
+
+}
+void executeStb(instruction i){
+
+}
+void executeLod(instruction i){
+
+}
+void executeJmp(instruction i){
+
+}
+void executeJlt(instruction i){
+
+}
+void executeJeq(instruction i){
+
+}
+void executeCal(instruction i){
+
+}
+void executePsh(instruction i){
+
+}
+void executePop(instruction i){
+
+}
+void executeNot(instruction i){
+
+}
+void executeOut(instruction i){
+    //cout << "executing out" << endl;
+    if(i.firstcode == "100"){
+        //cout << stoi(i.val, NULL, 2);
+        //cout << registers[0];
+        //cout << (char)registers[stoi(i.val, NULL, 2)] << endl;
+        our_stdout += (char)registers[stoi(i.val, NULL, 2)];
+    }
+    else if(i.firstcode == "101"){
+        //might be wrong, i dont really care
+        our_stdout += (char)stoi(i.val, NULL, 2);
+    }
+    else{
+        cout << "problem with executeOut" << endl;
+    }
+
+}
+void executeInp(instruction i){
+
+}
+void executeAmp(instruction i){
+
+}
+void executeAlt(instruction i){
+
+}
+void executeAeq(instruction i){
+
+}
+void executeAal(instruction i){
+
+}
+void executeRet(instruction i){
+
+}
+void executeNop(instruction i){
+
+}
+
 
 
 void OrcRunner::printListOfStrings() const{
@@ -102,11 +228,11 @@ instruction interpretInstructionType1(std::string fullinstruction){
             i.name = "NAN";
             break;
         }
-    if(i.opcode == "010"){  
+    if(i.firstcode == "010"){  
         i.src = fullinstruction.substr(7,4);
         i.dest = fullinstruction.substr(11,4);
     }
-    else if(i.opcode == "011"){
+    else if(i.firstcode == "011"){
         i.src = fullinstruction.substr(14);
         i.dest = fullinstruction.substr(7,4);
     }
@@ -164,10 +290,10 @@ instruction interpretInstructionType2(std::string fullinstruction){
             i.name = "NAN";
             break;
         }
-    if(i.opcode == "100"){  
+    if(i.firstcode == "100"){  
         i.val = fullinstruction.substr(7,4);
     }
-    else if(i.opcode == "101"){
+    else if(i.firstcode == "101"){
         i.val = fullinstruction.substr(7);
     }
     return i;
@@ -221,10 +347,39 @@ size_t interpretAndWrite(std::string firstLineOfOper, size_t indexInMem, std::un
         printer << errorMsg << endl;
     }
 
-
-
-
     if(instructionParsed) printer << instr.toString() << endl;
+    if(!instructionParsed) return returnValue;
+    if(instr.name == "ADD") executeAdd(instr);
+    else if(instr.name == "SUB") executeSub(instr);
+    else if(instr.name == "MUL") executeMul(instr);
+    else if(instr.name == "DIV") executeDiv(instr);
+    else if(instr.name == "CMP") executeCmp(instr);
+    else if(instr.name == "TST") executeTst(instr);
+    else if(instr.name == "AND") executeAnd(instr);
+    else if(instr.name == "ORR") executeOrr(instr);
+    else if(instr.name == "XOR") executeXor(instr);
+    else if(instr.name == "STR") executeStr(instr);
+    else if(instr.name == "STB") executeStb(instr);
+    else if(instr.name == "LOD") executeLod(instr);
+    else if(instr.name == "JMP") executeJmp(instr);
+    else if(instr.name == "JLT") executeJlt(instr);
+    else if(instr.name == "JEQ") executeJeq(instr);
+    else if(instr.name == "CAL") executeCal(instr);
+    else if(instr.name == "PSH") executePsh(instr);
+    else if(instr.name == "POP") executePop(instr);
+    else if(instr.name == "NOT") executeNot(instr);
+    else if(instr.name == "OUT") executeOut(instr);
+    else if(instr.name == "INP") executeInp(instr);
+    else if(instr.name == "AMP") executeAmp(instr);
+    else if(instr.name == "ALT") executeAlt(instr);
+    else if(instr.name == "AEQ") executeAeq(instr);
+    else if(instr.name == "AAL") executeAal(instr);
+    else if(instr.name == "RET") executeRet(instr);
+    else if(instr.name == "NOP") executeNop(instr);
+    else{
+
+    }
+
     return returnValue;
 }
 
@@ -240,6 +395,10 @@ void OrcRunner::execute()  {
     ofs_full.open(desired_filename_full);
     ofs_limited.open(desired_filename_lim);
     printer.open("instructionLog.txt");
+
+    //set registers to an array full of zeros
+    registers = new int[11];
+    for(int i = 0; i < 11; i++) registers[i] = 0;
 
 
     // YO! You are allowed to do this! That way you don't have to do "MEMORY.at(40).value"
@@ -268,8 +427,12 @@ void OrcRunner::execute()  {
         else currentIndex = nextLoc;
     }
 
+    cout << "\nThis is our_stdout: " << our_stdout << endl << endl;
+
     ofs_full.close();
     ofs_limited.close();
     printer.close();
+
+    delete [] registers;
 }
 
